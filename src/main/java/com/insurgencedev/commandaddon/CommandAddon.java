bd1a900adcf0df02d81be8c46640b8ec9d3896b1
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-03-22 05:34:24 -0500

    api update + version bump

--- src/main/java/com/insurgencedev/commandaddon/CommandAddon.java
+++ src/main/java/com/insurgencedev/commandaddon/CommandAddon.java
@@ -5,7 +5,7 @@ import com.insurgencedev.commandaddon.commands.StopCmd;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
-@IBoostersAddon(name = "CommandAddon", version = "1.0.0", author = "InsurgenceDev", description = "Some extra comamnds!")
+@IBoostersAddon(name = "CommandAddon", version = "1.0.1", author = "InsurgenceDev", description = "Some extra comamnds!")
 public class CommandAddon extends InsurgenceBoostersAddon {
 
     @Override
